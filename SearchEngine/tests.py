5cc61816eef5087031eff5179025de580020e647
Saloni Gupta <saloni010@hotmail.com>
2017-02-08 13:14:09 +0800

    simple django app

--- SearchEngine/tests.py
+++ SearchEngine/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
 # Create your tests here.
+import datetime
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+from django.utils import timezone
+from .models import Question
+def create_question(question_text, days):
+   time = timezone.now() + datetime.timedelta(days=days)
+   return Question.objects.create(question_text=question_text, pub_date=time)
+
+class QuestionViewTests(TestCase):
+    def test_index_view_with_no_questions(self):
+        """
+        If no questions exist, an appropriate message should be displayed.
+        """
+        response = self.client.get(reverse('SearchEngine:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No SearchEngines are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
