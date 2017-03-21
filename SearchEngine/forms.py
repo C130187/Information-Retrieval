73299093ee7b634f322b578d40fb0ffc24292c0d
shuvamnandi <shuvamnandi9@gmail.com>
2017-03-22 03:02:15 +0800

    Created folder to store crawled data dumped into json files. Modified crawl.py to support crawling by query - this gets articles from both sport and football sections.

--- SearchEngine/forms.py
+++ SearchEngine/forms.py
@@ -3,6 +3,7 @@ from django import forms
 
 class CrawlForm(forms.Form):
     query = forms.CharField(label='Query keyword', max_length=100, required=False)
+    '''
     OPTIONS = [ (0, 'Crawl by Categories'),
                 (1, 'Crawl by Query'),
                 (2, 'Crawl all') ]
@@ -10,4 +11,6 @@ class CrawlForm(forms.Form):
     CATEGORIES = [(0, 'Football'),
                   (1, 'Sports')
                   ]
+
     crawlSelection = forms.MultipleChoiceField(CATEGORIES, False, forms.CheckboxSelectMultiple)
+    '''
