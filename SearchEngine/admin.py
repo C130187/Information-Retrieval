7e48a0562dd5c241dacefb140930b1e7dd024bf0
Shuvam Nandi <shuvamnandi9@gmail.com>
2017-04-04 23:07:41 +0800

    Further cleanup

--- SearchEngine/admin.py
+++ SearchEngine/admin.py
@@ -1,17 +1,1 @@
 from django.contrib import admin
-
-# Register your models here.
-from .models import Choice, Question
-
-class ChoiceInline(admin.TabularInline):
-    model = Choice
-    extra = 3
-
-class QuestionAdmin(admin.ModelAdmin):
-    fieldsets = [
-            (None,               {'fields': ['question_text']}),
-            ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
-                ]
-    inlines = [ChoiceInline]
-
-admin.site.register(Question, QuestionAdmin)
